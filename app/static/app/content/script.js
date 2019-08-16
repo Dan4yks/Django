fa7fabcb9e0972127b98a4827c82edcb5311528e
Daniil Sokolov <dan28012000@gmail.com>
2019-08-16 22:21:21 +0500

    Version1.8

--- app/static/app/content/script.js
+++ app/static/app/content/script.js
@@ -1,4 +1,4 @@
-let btn = document.querySelector('.application.second .btn.btn-secondary.btn-block'),
+let btn = document.querySelector('.application.second .conv button'),
   text = document.querySelector('.application.second .result input'),
   input = document.querySelector('.application.second .start input'),
   currency = document.querySelectorAll('.application.second .start a'),
@@ -8,8 +8,10 @@ let btn = document.querySelector('.application.second .btn.btn-secondary.btn-blo
 currency.forEach(function (element) {
   element.addEventListener('click', function () {
     start = element.textContent;
+    input.value += ' ' + start;
   });
 });
+
 currency2.forEach(function (element) {
   element.addEventListener('click', function () {
     result = element.textContent;
@@ -21,21 +23,21 @@ btn.addEventListener('click', function () {
     'http://data.fixer.io/api/latest?access_key=afcc2a59554340029d77f0a6a690e74a&format=1',
     function (data) {
       let money, money_after_conversion, new_currency;
-      money = input.value;
+      money = input.value.replace(/ .*/, "");
       if (start != 'EUR') {
 
         if (data['rates'][start] > 1) {
           new_currency = money / +data['rates'][start];
           money_after_conversion = new_currency * data['rates'][result];
-          text.value = money_after_conversion.toFixed(1);
+          text.value = money_after_conversion.toFixed(1) + ' ' + result;
         } else {
           new_currency = money * +data['rates'][start];
           money_after_conversion = new_currency * data['rates'][result];
-          text.value = money_after_conversion.toFixed(1);
+          text.value = money_after_conversion.toFixed(1) + ' ' + result;
         }
       } else {
         money_after_conversion = money * +data['rates'][result];
-        text.value = money_after_conversion.toFixed(1);
+        text.value = money_after_conversion.toFixed(1) + ' ' + result;
       }
     }
   )
